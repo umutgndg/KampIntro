b6dc6f396fefaac9f310c216143e909d9078cb3c
umutg <umutg@UMUT>
2021-10-30 20:38:05 +0300

    Add project files.

---
+++ ClassIntro/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ClassIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string adi = "Engin";
+            int yas = 36;
+
+            Kurs kurs1=new Kurs();
+            kurs1.KursAdı = "C#";
+            kurs1.Egitmen = "Engin Demiroğ";
+            kurs1.IzlenmeOrani = 68;
+
+            Kurs kurs2 = new Kurs();
+            kurs2.KursAdı = "Java";
+            kurs2.Egitmen = "Kerem Varış";
+            kurs2.IzlenmeOrani = 64;
+
+            Kurs kurs3 = new Kurs();
+            kurs3.KursAdı = "Python";
+            kurs3.Egitmen = "Berkan Bilgin";
+            kurs3.IzlenmeOrani = 80;
+
+            //Console.WriteLine(kurs1.KursAdı + " " + kurs1.Egitmen);
+
+            Kurs[] kurslar = new Kurs[] { kurs1, kurs2, kurs3 };
+
+            foreach (var kurs in kurslar)
+            {
+                Console.WriteLine(kurs.KursAdı);
+            }
+        }
+    }
+
+    class Kurs
+    {
+        public string KursAdı { get; set; }
+        public string Egitmen { get; set; }
+        public int IzlenmeOrani { get; set; }
+    }
+}
