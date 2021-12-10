23586381b59321b6f132cf2cddf04d4cf9d31570
umutg <umutg@UMUT>
2021-12-10 17:49:17 +0300

    10 Aralık 2021

---
+++ GameProject/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GameProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GamerManager gamerManager = new GamerManager(new NewEStateValidationManager());
+            gamerManager.Add(new Gamer
+            {
+                ID = 1,
+                BirthYear = 1985,
+                FirstName = "ENGİN",
+                LastName = "DEMİROĞ",
+                IdentityNumber = 12345
+            });
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
